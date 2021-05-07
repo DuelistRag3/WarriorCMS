72726f557824c05364967715dfe796a4b4dda644
Christian Dullin <christian.dullin@web.de>
2021-05-07 18:16:39 +0200

    Installer Basic

---
+++ app/Modules/Install/Views/start.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="/app/Themes/installer/style.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <p>
+            Wellcome to the WarriorCMS version <?= $version ?>
+          </p>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
