0295b3c1cca0f6c153edb7d9a8ccfa40c7a4a9a7
Christian Dullin <christian.dullin@web.de>
2021-05-07 19:10:47 +0200

    Prepare new theming

--- app/Modules/Install/Controllers/Install.php
+++ app/Modules/Install/Controllers/Install.php
@@ -8,10 +8,14 @@ namespace Modules\Install\Controllers;
 
 class Install extends \CodeIgniter\Controller
 {
+    public function __construct() {
+    }
+
     public function index() {
       $data = [
         'version' => '0.1',
       ];
+
       echo view('Modules\Install\Views\start', $data);
     }
 }
--- app/Modules/Install/Views/start.php
+++ app/Modules/Install/Views/start.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <link rel="stylesheet" href="/app/Themes/installer/style.css">
   </head>
   <body>
     <div class="container">
       <div class="row">
         <div class="col">
-          <p>
-            Wellcome to the WarriorCMS version <?= $version ?>
-          </p>
+          <p> Wellcome to the WarriorCMS version <?= $version ?> </p>
         </div>
       </div>
     </div>
