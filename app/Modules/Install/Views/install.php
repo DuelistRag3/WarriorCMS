72601eb7b6aeed0dffd8b60abd95798b7890ddfc
Christian Dullin <christian.dullin@web.de>
2021-05-10 20:10:56 +0200

    Containered the logo

--- app/Modules/Install/Views/install.php
+++ app/Modules/Install/Views/install.php
@@ -5,8 +5,10 @@
   </head>
   <body>
     <div class="inline">
-      <img class="logo" src="/themes/installer/img/logo.png" width="100px;" height="100px;"/>
-      <div class="container pan-1" id="startpan">
+      <div class="logo-container">
+        <img class="logo" src="/themes/installer/img/logo.png"/>
+      </div>
+      <div class="container" id="startpan">
         <div class="row">
           <div class="col">
             <div class="head">
@@ -17,7 +19,22 @@
                In the next steps you will install the CMS, it´s requiered you have already setuped your WoW Server <br />
                and created an empty database for the CMS
              </p>
-            <button class="btn" id="startnextbtn" onclick="changeSection('startpan', 'generalpan')">Next</button>
+            <button class="btn" id="" onclick="next('startpan', 'genpan')">Next</button>
+          </div>
+        </div>
+      </div>
+      <div class="container hidden-panel" id="genpan">
+        <div class="row">
+          <div class="col">
+            <div class="head">
+              <h1 class="lc">Installation</h1>
+            </div>
+            <p class="gi">
+               Welcome to the WarriorCMS version <?= $version ?> installation. <br />
+               In the next steps you will install the CMS, it´s requiered you have already setuped your WoW Server <br />
+               and created an empty database for the CMS
+             </p>
+            <button class="btn" id="" onclick="next('genpan', '')">Next</button>
           </div>
         </div>
       </div>
@@ -26,5 +43,11 @@
 </html>
 
 <script>
+function next(curpanel, nextpanel) {
 
+}
+
+function prev(curpanel, prevpanel) {
+
+}
 </script>
