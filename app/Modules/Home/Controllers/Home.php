1ee10b6669ae275e8177c5198edf627f17d702a2
Christian Dullin <christian.dullin@web.de>
2021-05-26 17:27:11 +0200

    Moving styling to correct folder

--- app/Modules/Home/Controllers/Home.php
+++ app/Modules/Home/Controllers/Home.php
@@ -6,8 +6,17 @@
 
 namespace Modules\Home\Controllers;
 
+use Arifrh\Themes\Themes;
+
 class Home extends \CodeIgniter\Controller
 {
+    public function __construct() {
+      $theme = config('default');
+      Themes::init($theme)
+      ->addCSS('style.css')
+      ->addCSS('home.css');
+    }
+
     public function index() {
         echo view('Modules\Home\Views\home');
     }
--- app/Modules/Home/Views/home.php
+++ app/Modules/Home/Views/home.php
@@ -3,9 +3,9 @@
 
 <head>
 
-    <style>
-        <?php include 'app/Modules/Home/Assets/css/home.css'; ?>
-    </style>
+  <head>
+    <?php Arifrh\Themes\Themes::renderCSS('default'); ?>
+  </head>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="/https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
 
