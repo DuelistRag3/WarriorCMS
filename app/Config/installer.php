50450b4d987f589f174107eb7db2dd630ca51375
DuelistRag3 <christian.dullin@web.de>
2022-04-02 20:25:42 +0200

    Update

--- app/Config/installer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Config;
-
-class installer extends \Arifrh\Themes\Config\Themes
-{
-	public $theme = 'installer';
-
-}
--- app/Modules/Home/Config/Routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/**
- * Define Install Routes
- */
-$routes->get('home', '\Modules\Home\Controllers\Home::index',['filter' => 'install']);
